a88113c04f0c6fcb739601b25cdbb0063d0e90c8
LaxmanChoudhary <laxmanchoudhary6811@gmail.com>
2021-10-13 12:28:20 +0530

    preview1

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,5 +2,5 @@
 // https://nextjs.org/learn/basics/api-routes/api-routes-details
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'Hello' })
+  res.status(200).json({ name: 'Hello this is jackson' })
 }
